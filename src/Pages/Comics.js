43372eca6e9c38896f7c160cacd7a1a65f3fde82
MatthieuTricoire <matthieutricoire@gmail.com>
2023-02-19 11:06:55 +0000

    first commit marvel front - no login

--- src/Pages/Comics.js
+++ src/Pages/Comics.js
@@ -27,7 +27,7 @@ const Comics = ({ favCom, setFavCom, handleFavorites }) => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `marvel-backend:8080?title=${searchComic}&skip=${
+          `site--marvel-backend--vmph8rxlwjhj.code.run/comics?title=${searchComic}&skip=${
             (pageNbComics - 1) * nbItems
           }&limit=${nbItems}`
         );
